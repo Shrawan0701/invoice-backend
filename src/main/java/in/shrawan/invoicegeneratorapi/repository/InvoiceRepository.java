c2a6f427c3dc4303df23f4ba69f88e0d57e95b18
Shrawan <shrawanre07@gmail.com>
2025-07-22 19:31:27 +0530

    pushed

---
+++ src/main/java/in/shrawan/invoicegeneratorapi/repository/InvoiceRepository.java
@@ -0,0 +1,14 @@
+package in.shrawan.invoicegeneratorapi.repository;
+
+import in.shrawan.invoicegeneratorapi.entity.Invoice;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InvoiceRepository extends MongoRepository<Invoice, String> {
+
+    List<Invoice> findByClerkId(String id);
+
+    Optional<Invoice> findByClerkIdAndId(String clerkId, String id);
+}
