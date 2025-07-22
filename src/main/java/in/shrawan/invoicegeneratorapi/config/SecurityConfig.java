b608feb00cad4e91b66c8d6fc85878f7bd290bd1
Shrawan <shrawanre07@gmail.com>
2025-07-22 23:28:13 +0530

    update cors

--- src/main/java/in/shrawan/invoicegeneratorapi/config/SecurityConfig.java
+++ src/main/java/in/shrawan/invoicegeneratorapi/config/SecurityConfig.java
@@ -42,8 +42,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://quick-invoice-generator-seven.vercel.app"
+        config.setAllowedOrigins(List.of("*"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
